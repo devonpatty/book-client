7b1fd8474a970ddfa39e7688076d52d36c4b4a82
Hoai Nam Duc Tran <kristjantran@gmail.com>
2018-04-11 09:17:29 +0000

    programming auth.js

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -7,6 +7,9 @@
 import api from '../api';
 
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const LOGIN_LOGOUT = 'LOGIN_LOGOUT';
 
 function requestLogin() {
   return {
@@ -19,4 +22,31 @@ function requestLogin() {
 
 /* todo fleiri action */
 
+function receiveLogin(user) {
+  return {
+    type: LOGIN_REQUEST,
+    isFetching: false,
+    isAuthenticated: true,
+    user,
+    message: null,
+  }
+}
+
+function loginError(message) {
+  return {
+    type: LOGIN_FAILURE,
+    isFetching: false,
+    isAuthenticated: false,
+    message,
+  }
+}
+
+function logout() {
+  return {
+    type: LOGIN_LOGOUT,
+    isFetching: false,
+    isAuthenticated: false,
+    user: null,
+  }
+}
 /* todo async "thunk" fyrir tengingu við vefþjónustu */
