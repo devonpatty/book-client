c6a095d710eb0966c16500c7b580a898d24cd2a6
unknown <jonrunar1995@gmail.com>
2018-04-19 23:56:03 +0000

    breyta bók og lesa bók virkni bætt við

--- src/routes/bookEdit/BookEdit.js
+++ src/routes/bookEdit/BookEdit.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router';
 import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import axios from 'axios';
 
 import './BookEdit.css';
 
 import Button from "../../components/button/index";
 
+const baseurl = process.env.REACT_APP_SERVICE_URL;
+
 class BookEdit extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: true, 
       id: this.props.location.state.book.bookid,
       title: this.props.location.state.book.title,
       isbn13: this.props.location.state.book.isbn13,
@@ -20,6 +25,7 @@ class BookEdit extends Component {
       published: this.props.location.state.book.published,
       pagecount: this.props.location.state.book.pagecount,
       language: this.props.location.state.book.language,
+      allCategories: {},
     };
 
     this.changeTitle = this.changeTitle.bind(this);
@@ -32,6 +38,25 @@ class BookEdit extends Component {
     this.changePagecount = this.changePagecount.bind(this);
     this.changeLanguage = this.changeLanguage.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.getCategories = this.getCategories.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  async componentDidMount() {
+    try {
+      const data = await this.fetchData();
+      this.setState({ loading: false, allCategories: data});
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
+  async fetchData() {
+    const response = await axios.get(
+      `${baseurl}categories`,
+    )
+    return response;
+    
   }
 
   changeTitle(event) {
@@ -62,40 +87,69 @@ class BookEdit extends Component {
     this.setState({language: event.target.value});
   }
 
-  handleSubmit(event) {
-    const URL = 'https://djbook.herokuapp.com/books/:id'
-    const {
-      bookid,
-      title,
-      isbn13,
-      author,
-      description,
-      category,
-      isbn10,
-      published,
-      pagecount,
-      language,
-  } = this.state
-    axios.patch(URL , {
-      bookid,
-      title,
-      isbn13,
-      author,
-      description,
-      category,
-      isbn10,
-      published,
-      pagecount,
-      language,
-    }).then((response) => {
-      alert('A Patch update was submitted: ' + response);
-      event.preventDefault();
-    })
+  getCategories(categories) {
+    const cat = categories.data.map((category) => 
+      <option key={category.cateid} value={category.cateid}> {category.name} </option>
+    )
+    return cat;
 
   }
 
-  render() {
+  handleSubmit(event) {
+    const { book } = this.props.location.state;
+    const token = window.localStorage.getItem('token');
+    const parsedToken = JSON.parse(token);
+    
     const {
+      bookid,
+      title,
+      isbn13,
+      author,
+      description,
+      category,
+      isbn10,
+      published,
+      pagecount,
+      language,
+  } = this.state;
+
+  axios({
+    method: 'PATCH',
+    url: `${baseurl}books/${book.bookid}`,
+    data: {
+      bookid,
+      title,
+      isbn13,
+      author,
+      description,
+      category,
+      isbn10,
+      published,
+      pagecount,
+      language,
+    },
+    headers: { Authorization: `Bearer ${parsedToken}` },
+  })
+  .then((response) => {
+    alert("Bók hefur verið breytt");
+    this.props.history.push('/books');
+  })
+  .catch((err) => {
+    if (err.response) {
+      alert(err.response.data.error);
+    }
+  });
+
+  }
+
+  onClick() {
+    this.props.history.push('/books');
+  }
+
+  render() {
+
+    const {
+      loading,
         bookid,
         title,
         isbn13,
@@ -106,13 +160,16 @@ class BookEdit extends Component {
         published,
         pagecount,
         language,
+        allCategories,
     } = this.state
 
-    const path = 'https://vefforritun2-2018-v4-synilausn.herokuapp.com/'+bookid;
+    if (loading) {
+      return (<div>Hleð inn gögnum...</div>);
+    }
+
     return (
       <div>
         <h2 className="margin_div">Breyta Bók</h2>
-        {/*<form method='PATCH' action={path}>*/}
         <div className="change_div">
           <form>
             <div className="margin_div">
@@ -151,7 +208,9 @@ class BookEdit extends Component {
                   Flokkur:  
                 </div>
                 <div>
-                  <input type='text' value={category} onChange={this.changeCategory}/>  
+                  <select name="category" value={category} onChange={this.changeCategory}>
+                    { this.getCategories(allCategories) }
+                  </select>
                 </div>
               </label>
             </div>
@@ -209,7 +268,12 @@ class BookEdit extends Component {
           <Button className="btn_save" onClick={this.handleSubmit}>Vista</Button>
         </div>
         <div>
-          <Button>Til baka</Button>
+          <button>
+            <Link to={{
+              pathname:`/books/${ this.props.location.state.book.bookid}`,
+              state: { book:  this.props.location.state.book }
+            }}> Til baka! </Link>
+          </button>
         </div>
       </div>
     );
